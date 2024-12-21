076a640ed00450f4d780f0eee1aa9ad921848595
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2024-12-21 05:25:35 +0000

    Format

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ async fn hello_world() -> &'static str {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(hello_world));
+    let app = Router::new().route("/", get(hello_world));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
     println!("Server running on http://127.0.0.1:3000");
 
     axum::serve(listener, app).await.unwrap();
